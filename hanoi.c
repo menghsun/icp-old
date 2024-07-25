6f2240ce7c1e4f68b466f6ba0d7326ec4da9054d
menghsun <tsaimh@cs.nycu.edu.tw>
2024-07-26 06:37:26 +0800

    add hanoi.c

---
+++ hanoi.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+void hanoi_tower(int n,char a,char b,char c)
+{
+   if(n == 1){
+      printf("[Disk %d] %c -> %c\n", n, a, c);
+   }
+   else{
+      hanoi_tower(n-1,a,c,b);
+      printf("[Disk %d] %c -> %c\n", n, a, c);
+      hanoi_tower(n-1,b,a,c);
+   }
+}
+int main()
+{
+   int n;
+   printf("Input number of disks: ");
+   scanf("%d",&n);
+   hanoi_tower(n,'A','B','C’);
+   return 0;
+}
+
